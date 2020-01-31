f2b7a6396c41b5017d58e699e2b89519b81c0825
Gabriel Debona <gabriel.debona@gmail.com>
2020-01-31 02:31:27 -0300

    Create layout mode atom

--- src/components/atoms/a-layout-mode.vue
+++ src/components/atoms/a-layout-mode.vue
@@ -1,2 +1,67 @@
 <template>
+  <div id="layout-mode" @click="toggleMode()">
+    {{ changeTo }} Mode
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'ALayoutMode',
+  data() {
+    return {
+      darkMode: true,
+      templates: [
+        {
+          name: 'Dark',
+          background: '#202c37',
+          elColor: '#2b3945',
+          textColor: '#ffffff',
+          shadow: '#252525',
+        },
+        {
+          name: 'Light',
+          background: '#fafafa',
+          elColor: '#ffffff',
+          textColor: '#111517',
+          shadow: '#eeeeee',
+        },
+      ],
+    };
+  },
+  computed: {
+    changeTo() {
+      if (this.darkMode) return this.templates[1].name;
+      return this.templates[0].name;
+    },
+  },
+  methods: {
+    currentTemplate() {
+      const template = this.darkMode ? 'Dark' : 'Light';
+      return this.templates.find(({ name }) => name === template);
+    },
+    toggleMode() {
+      this.darkMode = !this.darkMode;
+      this.setNewTemplate();
+    },
+    setNewTemplate() {
+      const currentTemplate = this.currentTemplate();
+
+      document.documentElement.style
+        .setProperty('--primary-background-color', currentTemplate.background);
+      document.documentElement.style
+        .setProperty('--primary-el-color', currentTemplate.elColor);
+      document.documentElement.style
+        .setProperty('--primary-text-color', currentTemplate.textColor);
+      document.documentElement.style
+        .setProperty('--primary-shadow-color', currentTemplate.shadow);
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  #layout-mode {
+    font-weight: 600;
+    cursor: pointer;
+  }
+</style>
