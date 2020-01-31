7957cb8c72c922e0eba508e1d150c71fe647f787
Gabriel Debona <gabriel.debona@gmail.com>
2020-01-31 02:28:59 -0300

    Add header molecule

---
+++ src/components/molecules/m-header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="countries-header">
+    <div class="container grid-xl">
+      <div class="columns">
+        <div class="column col-12">
+          <div class="header__content">
+            <h1>Where in the world?</h1>
+            <a-layout-mode/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ALayoutMode from '../atoms/a-layout-mode.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ALayoutMode,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .countries-header {
+    background: var(--primary-el-color);
+    color: var(--primary-text-color);
+    padding: 25px 0;
+    box-shadow: 0 0 7px 1px var(--primary-shadow-color);
+
+    .header__content {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    h1 {
+      font-weight: 800;
+      font-size: 18px;
+      margin: 0;
+
+      @media (min-width: 480px) {
+        font-size: 26px;
+      }
+    }
+  }
+</style>
