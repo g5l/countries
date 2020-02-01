aaab5be2ea77ca60b090334f05159e2efde39fd0
Gabriel Debona <gabriel.debona@gmail.com>
2020-02-01 02:36:20 -0300

    Add a button

---
+++ src/components/atoms/a-button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button :class="{ 'small': small }" class="button">
+    <slot name="icon"></slot>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'AButton',
+  props: {
+    small: Boolean,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .button {
+    background: var(--primary-el-color);
+    color: var(--primary-text-color);
+    box-shadow: 0 0 7px 1px var(--primary-shadow-color);
+    font-size: 14px;
+    padding: 10px 25px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:focus {
+      outline: none;
+    }
+
+    &.small {
+      padding: 5px 25px;
+    }
+  }
+</style>
